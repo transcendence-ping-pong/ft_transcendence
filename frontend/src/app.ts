a712e7ebb58eb6b1e51f0d5ff98b754d0c4ac807
Sofia Hinckel Dias <sofiahdias@gmail.com>
2025-05-23 12:57:08 +0100

    feat: add state management

--- frontend/src/app.ts
+++ frontend/src/app.ts
@@ -1,9 +1,12 @@
 import { getTranslations } from './utils/translations.js';
 import { renderGamePage } from './pages/Game.js';
+import { state } from './state.js';
 
 document.addEventListener('DOMContentLoaded', async () => {
   // TODO: translations by default will be in english. Apply switching to other languages later
-  const translations = await getTranslations();
+  // TODO: populate state, watch for changes? (?)
+  state.translations = await getTranslations(state.language);
+
   const app = document.getElementById('app');
-  if (app) renderGamePage(app, translations);
+  if (app) renderGamePage(app);
 });
