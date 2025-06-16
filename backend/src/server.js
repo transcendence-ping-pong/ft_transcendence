c7564de74af81dc110aa4f1399a85b0d7f2354f5
mbraga-s <mbraga-s@student.42lisboa.com>
2025-06-16 19:55:07 +0100

    Lastest on fastify

---
+++ backend/src/server.js
@@ -0,0 +1,29 @@
+
+// Require the framework and instantiate it, as well as needed plugins
+const path = require('path');
+const fastify = require('fastify')({ logger: true })
+const fastifyStatic = require('@fastify/static');
+
+// Serve static files like index.html
+fastify.register(fastifyStatic, {
+  root: path.join(__dirname, 'src'),
+  prefix: '/',
+});
+
+// Handle form submissions
+fastify.post('/login', async (request, reply) => {
+  const { username, password } = request.body;
+
+	console.log('Form received:');
+	console.log('Username:', username);
+	console.log('Password:', password);
+	reply.send({ status: 'ok' });
+});
+
+// Run the server!
+fastify.listen({ port: 4000, host: '0.0.0.0' }, (err) => {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+})
