7f186368415bfa3de839b47fa5eea68edbc502a7
mbraga-s <miguel.soares.correia@outlook.com>
2025-07-05 20:00:03 +0100

    Added matches endpoint. Updated DB schema to used naming convention

---
+++ backend/api/matches.js
@@ -0,0 +1,109 @@
+
+const fastify = require('fastify')({ logger: true });
+const fastifyStatic = require('@fastify/static');
+const path = require('path');
+const sqlite3 = require('sqlite3').verbose();
+
+let html_path = path.join(path.dirname(__dirname), 'temp');
+
+fastify.register(fastifyStatic, {
+  root: html_path,
+  prefix: '/',
+});
+
+const port = 4000;
+
+let db_path = path.join(path.dirname(__dirname), '/database/database.db');
+
+const db = new sqlite3.Database(db_path, sqlite3.OPEN_READWRITE);
+
+fastify.get('/matches', (request, reply) => {
+	db.all(`SELECT * FROM matchStats`, (err, rows) => {
+
+		if (err) {
+			return reply.status(500).send({ error: 'Error fetching matches from database' });
+		}
+		reply.send(rows);
+	});
+});
+
+fastify.post('/matches', (request, reply) => {
+	const {
+		matchType,
+		creatorUserId,
+		player1DisplayName,
+		player2DisplayName,
+		winnerDisplayName,
+		scorePlayer1,
+		scorePlayer2,
+		forfeit
+	} = request.body;
+	
+	// TODO: Verify if additional checks are needed
+	if (!creatorUserId) {
+		return reply.status(400).send({ error: 'Incomplete player credentials.' });
+	}
+
+	const d = new Date();
+	
+	function addZero(i) {
+		if (i < 10) {i = "0" + i}
+		return i;
+	}
+	
+	let day = addZero(d.getDate());
+	let month = addZero(d.getMonth() + 1);
+	let year = addZero(d.getFullYear());
+	let h = addZero(d.getHours());
+	let m = addZero(d.getMinutes());
+	let s = addZero(d.getSeconds());
+
+	const time = h + ":" + m + ":" + s;
+	const date = day + "/" + month + "/" + year;
+
+	db.run(`INSERT INTO matchStats (matchType, creatorUserId, player1DisplayName, player2DisplayName,
+		 winnerDisplayName, scorePlayer1, scorePlayer2, forfeit, date, time) 
+		VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`, [matchType, creatorUserId, player1DisplayName, 
+			player2DisplayName, winnerDisplayName, scorePlayer1, scorePlayer2, forfeit, date, time], function (err) {
+		if (err) {
+			return reply.status(500).send({ error: 'Error adding match to database' });
+		}
+		
+		const matchId = this.lastID;
+		
+		db.run(`INSERT INTO matchHistory (userId, matchId) VALUES (?, ?)`, [creatorUserId, matchId], function (err) {
+			if (err) {
+				return reply.status(500).send({ error: 'Error adding match to user history' });
+			}
+			reply.send({ message: 'Success' });
+		});
+	});
+});
+
+fastify.get('/matches/:matchId', (request, reply) => {
+	const { matchId } = request.params;
+
+	db.get(`SELECT * FROM matchStats WHERE matchId = ?`, matchId, (err, rows) => {
+		if (err) {
+			return reply.status(500).send({ error: 'Error fetching match from database' });
+		}
+		reply.send(rows);
+	});
+});
+
+fastify.get('/matches/history/:userId', (request, reply) => {
+	const { userId } = request.params;
+
+	db.get(`SELECT * FROM matchHistory WHERE userId = ?`, userId, (err, rows) => {
+		if (err) {
+			return reply.status(500).send({ error: 'Error fetching user history from database' });
+		}
+		reply.send(rows);
+	});
+});
+
+fastify.listen({ port: port, host: '0.0.0.0' }, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
+
+fastify.get('/favicon.ico', (req, res) => res.status(204));
--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -7,41 +7,54 @@ CREATE TABLE IF NOT EXISTS users (
 	email TEXT
 );
 
-CREATE TABLE IF NOT EXISTS user_stats (
-	user_id INTEGER NOT NULL PRIMARY KEY,
-	username TEXT NOT NULL,
-	displayname TEXT,
+-- CREATE TABLE IF NOT EXISTS users (
+-- 	userId INTEGER PRIMARY KEY AUTOINCREMENT,
+-- 	username TEXT NOT NULL UNIQUE,
+-- 	password TEXT NOT NULL,
+-- 	secret TEXT NOT NULL,
+-- 	googleId TEXT UNIQUE,
+-- 	email TEXT
+-- );
+
+CREATE TABLE IF NOT EXISTS userStats (
+	userId INTEGER NOT NULL PRIMARY KEY,
+	displayName TEXT,
 	avatar TEXT NOT NULL DEFAULT 'placeholder',
-	user_status INTEGER DEFAULT 0,
-	match_count INTEGER DEFAULT 0,
-	win_count INTEGER DEFAULT 0,
-	loss_count INTEGER DEFAULT 0,
-	tornament_win_count INTEGER DEFAULT 0
+	userStatus INTEGER DEFAULT 0,
+	matchCount INTEGER DEFAULT 0,
+	winCount INTEGER DEFAULT 0,
+	lossCount INTEGER DEFAULT 0,
+	tornamentWinCount INTEGER DEFAULT 0,
+	FOREIGN KEY (userId) REFERENCES users(userId)
 );
 
-CREATE TABLE IF NOT EXISTS friend_list (
-	user_id INTEGER NOT NULL,
-	friend_id INTEGER NOT NULL,
-	friendship_status TEXT,
-	FOREIGN KEY (user_id) REFERENCES users(user_id),
-	FOREIGN KEY (friend_id) REFERENCES users(user_id)
+CREATE TABLE IF NOT EXISTS friendList (
+	userId INTEGER NOT NULL,
+	friendId INTEGER NOT NULL,
+	friendStatus TEXT DEFAULT 'pending',
+	FOREIGN KEY (userId) REFERENCES users(userId),
+	FOREIGN KEY (friendId) REFERENCES users(userId)
 );
 
-CREATE TABLE IF NOT EXISTS match_history (
-	user_id INTEGER NOT NULL,
-	match_id INTEGER NOT NULL,
-	FOREIGN KEY (user_id) REFERENCES users(user_id),
-	FOREIGN KEY (match_id) REFERENCES match_stats(match_id)
+
+CREATE TABLE IF NOT EXISTS matchHistory (
+	userId INTEGER NOT NULL,
+	matchId INTEGER NOT NULL,
+	FOREIGN KEY (userId) REFERENCES users(userId),
+	FOREIGN KEY (matchId) REFERENCES matchStats(matchId)
 );
 
-CREATE TABLE IF NOT EXISTS match_stats (
-	match_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	player1_id INTEGER NOT NULL,
-	player2_id INTEGER NOT NULL,
-	score_player1 INTEGER,
-	score_player2 INTEGER,
+CREATE TABLE IF NOT EXISTS matchStats (
+	matchId INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	creatorUserId INTEGER NOT NULL,
+	player1DisplayName TEXT NOT NULL,
+	player2DisplayName TEXT NOT NULL DEFAULT 'Bot',
+	winnerDisplayName TEXT NOT NULL,
+	scorePlayer1 INTEGER,
+	scorePlayer2 INTEGER,
+	matchType TEXT NOT NULL DEFAULT 'bot',
 	forfeit INTEGER,
-	winnerId INTEGER,
 	date INTEGER,
-	time INTEGER
+	time INTEGER,
+	FOREIGN KEY (creatorUserId) REFERENCES users(userId)
 );
