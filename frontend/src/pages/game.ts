d3746f6adebf3bdee8c881f88bfd91c6d1c3cfc2
Sofia Hinckel Dias <sofiahdias@gmail.com>
2025-07-10 18:22:04 +0100

    feat: add game border

--- frontend/src/pages/game.ts
+++ frontend/src/pages/game.ts
@@ -3,6 +3,22 @@ import '@/components/ThemeToggle.js'; // This registers <toggle-switch>
 import '@/components/DynamicDropdown.js';
 import '@/components/MenuNavigation.js';
 
+// TODO FIX: use scale-100 if want to scale down border image
 export function renderGame(containerId: string) {
-  new gameOrchestrator(containerId);
+  document.body.classList.add('overflow-hidden'); // prevent scrolling
+
+  const container = document.getElementById(containerId);
+  if (container) {
+    container.innerHTML = `
+      <div class="game-area">
+        <div id="game-screen"></div>
+        <img 
+          src="/public/game_border.png" 
+          alt="TV Frame"
+          class="absolute top-0 left-0 w-full h-full z-20 pointer-events-none"
+        />
+      </div>
+    `;
+    new gameOrchestrator('game-screen');
+  }
 }
