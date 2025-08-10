545e396e5aa98f01efbb8c72613aa9556c4d0513
mbraga-s <mbraga-s@student.42lisboa.com>
2025-08-10 18:37:07 +0100

    Finished tournament logic

--- backend/api/utils.js
+++ backend/api/utils.js
@@ -1,25 +1,45 @@
 
 function splitIntoRandomPairs(inputArray) {
-  if (inputArray.length !== 8) {
-    throw new Error("Input array must contain exactly 8 elements.");
-  }
+	if (inputArray.length !== 8) {
+		throw new Error("Input array must contain exactly 8 elements.");
+	}
 
-  // Shuffle the array using Fisher-Yates algorithm
-  const copyArray = [...inputArray]
-  for (let i = copyArray.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [copyArray[i], copyArray[j]] = [copyArray[j], copyArray[i]];
-  }
+	const copyArray = [...inputArray]
+	for (let i = copyArray.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[copyArray[i], copyArray[j]] = [copyArray[j], copyArray[i]];
+	}
 
-  // Split into 4 arrays of 2 elements each
-  const result = [];
-  for (let i = 0; i < 8; i += 2) {
-    result.push([copyArray[i], copyArray[i + 1]]);
-  }
+	const result = [];
+	for (let i = 0; i < 8; i += 2) {
+		result.push([copyArray[i], copyArray[i + 1]]);
+	}
 
-  return result;
+	return result;
+};
+
+function dbRun(db, sql, params) {
+	return new Promise((resolve, reject) => {
+		db.run(sql, params, function (err) {
+			if (err) return reject(err);
+			resolve({ lastID: this.lastID });
+		});
+	});
+};
+
+function getWinner(db, params) {
+	return new Promise((resolve, reject) => {
+		db.get('SELECT winnerDisplayName FROM matches WHERE matchId = ?', params, (err, row) => {
+			if (err) {
+				return reject(err);
+			} 
+			resolve({ name: row.winnerDisplayName });
+		});
+	});
 };
 
 module.exports = {
-  splitIntoRandomPairs,
+	splitIntoRandomPairs,
+	dbRun,
+	getWinner
 };
